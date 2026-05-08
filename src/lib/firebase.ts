8b9251197998486cbac338f0cb80daaa62ae51ee
anaaonoo2-sudo <anaaonoo2@gmail.com>
2026-05-08 22:57:40 +0100

    Refactor Firebase setup to use getFirestore

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,11 +1,19 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
-import { initializeFirestore } from 'firebase/firestore';
-import firebaseConfig from '../../firebase-applet-config.json';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAwgUr4WOwRbZEyMonnfhUqyovieEVZJAY",
+  authDomain: "boycash-dc4e4.firebaseapp.com",
+  projectId: "boycash-dc4e4",
+  storageBucket: "boycash-dc4e4.firebasestorage.app",
+  messagingSenderId: "367328224851",
+  appId: "1:367328224851:web:7668abe98d1a409b5cc8a6"
+};
 
 const app = initializeApp(firebaseConfig);
 
-export const db = initializeFirestore(app, {});
+export const db = getFirestore(app);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
 
