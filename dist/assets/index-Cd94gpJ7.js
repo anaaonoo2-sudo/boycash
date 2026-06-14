f49eb888bb4cd73007249ab8d8613bff9ed2fb6c
anaaonoo2-sudo <anaaonoo2-sudo@example.com>
2026-06-14 14:26:15 +0100

    fix: transparent status bar with padding

--- dist/assets/index-Cd94gpJ7.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./web-D8i3IkA_.js","./index-BSQlru7E.js","./index-BK5jd2on.css"])))=>i.map(i=>d[i]);
-import{r as o,_ as t}from"./index-BSQlru7E.js";const _=o("GoogleAuth",{web:()=>t(()=>import("./web-D8i3IkA_.js"),__vite__mapDeps([0,1,2]),import.meta.url).then(e=>new e.GoogleAuthWeb)});export{_ as GoogleAuth};
