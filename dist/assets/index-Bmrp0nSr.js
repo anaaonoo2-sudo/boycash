42fdd58811c94200ecfce14901c45efdf7f8d6a2
anaaonoo2-sudo <anaaonoo2-sudo@example.com>
2026-06-14 14:30:22 +0100

    fix: safe area padding for header

--- dist/assets/index-Bmrp0nSr.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./web-CRvX6Ray.js","./index-DdDVjtWj.js","./index-BK5jd2on.css"])))=>i.map(i=>d[i]);
-import{r as o,_ as t}from"./index-DdDVjtWj.js";const _=o("GoogleAuth",{web:()=>t(()=>import("./web-CRvX6Ray.js"),__vite__mapDeps([0,1,2]),import.meta.url).then(e=>new e.GoogleAuthWeb)});export{_ as GoogleAuth};
