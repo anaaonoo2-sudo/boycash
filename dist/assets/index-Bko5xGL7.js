00a613cf797b1689c82d046d003895065114cd6f
anaaonoo2-sudo <anaaonoo2-sudo@example.com>
2026-06-21 16:56:41 +0100

    splash screen محدث بالتصميم الجديد

---
+++ dist/assets/index-Bko5xGL7.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./web-C6jnxhK2.js","./index-B1vzdU0i.js","./index-C53tL3CH.css"])))=>i.map(i=>d[i]);
+import{r as o,_ as t}from"./index-B1vzdU0i.js";const _=o("GoogleAuth",{web:()=>t(()=>import("./web-C6jnxhK2.js"),__vite__mapDeps([0,1,2]),import.meta.url).then(e=>new e.GoogleAuthWeb)});export{_ as GoogleAuth};
