82d2b76212e731371d1e1e24103db69fb5a36a5b
anaaonoo2-sudo <anaaonoo2-sudo@example.com>
2026-07-04 22:13:34 +0100

    fix: arms rotate toward belly when thinking

---
+++ dist/assets/index-1xTxA6N4.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./web-dJFI8JMO.js","./index-O_659dF1.js","./index-8ck-ShvG.css"])))=>i.map(i=>d[i]);
+import{r as o,_ as t}from"./index-O_659dF1.js";const _=o("GoogleAuth",{web:()=>t(()=>import("./web-dJFI8JMO.js"),__vite__mapDeps([0,1,2]),import.meta.url).then(e=>new e.GoogleAuthWeb)});export{_ as GoogleAuth};
