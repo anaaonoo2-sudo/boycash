27b6bb1dd41c8036ac0113c577bebb86dc51835e
anaaonoo2-sudo <anaaonoo2-sudo@example.com>
2026-07-04 21:38:25 +0100

    fix: robot arms cross in front at 80deg

--- dist/assets/index-fwBppTg7.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./web-D7WKgbwb.js","./index-Dt23IB05.js","./index-8ck-ShvG.css"])))=>i.map(i=>d[i]);
-import{r as o,_ as t}from"./index-Dt23IB05.js";const _=o("GoogleAuth",{web:()=>t(()=>import("./web-D7WKgbwb.js"),__vite__mapDeps([0,1,2]),import.meta.url).then(e=>new e.GoogleAuthWeb)});export{_ as GoogleAuth};
