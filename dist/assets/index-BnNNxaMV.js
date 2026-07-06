66008797ae09cc1a39b29ec9fc0640081b601611
anaaonoo2-sudo <anaaonoo2-sudo@example.com>
2026-07-06 12:11:56 +0100

    fix: white bg for Morocco and SEPA icons only

---
+++ dist/assets/index-BnNNxaMV.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./web--mA2S2tD.js","./index-v6nLyb60.js","./index-BNpVXVEc.css"])))=>i.map(i=>d[i]);
+import{r as o,_ as t}from"./index-v6nLyb60.js";const _=o("GoogleAuth",{web:()=>t(()=>import("./web--mA2S2tD.js"),__vite__mapDeps([0,1,2]),import.meta.url).then(e=>new e.GoogleAuthWeb)});export{_ as GoogleAuth};
--- dist/assets/index-d1ZjWbql.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./web-CdtqYaUR.js","./index-BvfCMK7L.js","./index-BNpVXVEc.css"])))=>i.map(i=>d[i]);
-import{r as o,_ as t}from"./index-BvfCMK7L.js";const _=o("GoogleAuth",{web:()=>t(()=>import("./web-CdtqYaUR.js"),__vite__mapDeps([0,1,2]),import.meta.url).then(e=>new e.GoogleAuthWeb)});export{_ as GoogleAuth};
