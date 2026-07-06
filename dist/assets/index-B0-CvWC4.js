2983b0bf672bb531ad5ceab19dd5417c34649434
anaaonoo2-sudo <anaaonoo2-sudo@example.com>
2026-07-06 13:10:27 +0100

    fix: clip top 25% of arms to hide overlap above shoulders

--- dist/assets/index-B0-CvWC4.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./web-CvIdXcvg.js","./index-CygyVuDI.js","./index-BNpVXVEc.css"])))=>i.map(i=>d[i]);
-import{r as o,_ as t}from"./index-CygyVuDI.js";const _=o("GoogleAuth",{web:()=>t(()=>import("./web-CvIdXcvg.js"),__vite__mapDeps([0,1,2]),import.meta.url).then(e=>new e.GoogleAuthWeb)});export{_ as GoogleAuth};
